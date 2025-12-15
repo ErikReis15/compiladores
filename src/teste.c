93db57895217e506d664bdbe77fa1833cc5b1441
Vitor-Willian <vitor.peneluppi@unifesp.br>
2025-12-14 22:08:11 -0300

    Lex alterado

--- src/teste.c
+++ src/teste.c
@@ -1,27 +1,12 @@
-int gcd(int u, int v){
-    int a;
-    int b;
-    if (v == 0) 
-        return u;
-    else 
-        return gcd(v, u - (u/v) * v);
+int gcd(int u, int v)
+{
+if (v == 0) return u;
+else return gcd(v, u - (u/v) * v);
 }
-
-
 void main(void)
 {
-    int a;
-    int x;
-    int v[3];
-    a = x + gcd(4,0)/2;
-    2 + 1;
-    v[1] = 3;
-    v[5] = 2;
-    output(a);
-    if(a > 0){
-        int b;
-        b = 2;
-    }
-    x = b;
-return;
+int x; int y;
+x = input();
+y = input();
+output(gcd(x, y));
 }
