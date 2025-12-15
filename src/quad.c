c251ce0017a8e500d495de2c2c2555bc62888c14
felipe.alves16 <felipe.alves16@unifesp.br>
2025-12-14 22:28:13 -0300

    limpei o codigo

--- src/quad.c
+++ src/quad.c
@@ -26,8 +26,19 @@ void printTAC(AST *n, int nivel, int *reg, int *label, int *param);
 void printOP(AST *n, int *reg, int first, int *label, int *param){
     if (!n) return;
     int reg_ant, reg_ant2 = -1, lado = 0;
-    if(n->tipo == SEQ){
-        printf("seq");
+    
+    if(n->tipo == ID){
+        if(n->direita){
+
+            //printOP(n->direita, reg, first, label, param);
+            
+            // printTAC(n->direita, 0, reg, label, param);
+            // printf("t%d = t%d", *reg, *reg-1);
+            // *reg = *reg + 1;
+            // printf(" * 4\n");
+            // printf("t%d = ", *reg - 1);
+            // *reg = *reg + 1;
+        }
     }
     if(n->tipo == CHAMADA){
         if(first == -1){
@@ -57,13 +68,20 @@ void printOP(AST *n, int *reg, int first, int *label, int *param){
     reg_ant = *reg;
     printOP(n->esquerda, reg, 0, label, param);
     if(n->direita){
+        if(n->direita->tipo == ID){
+            if(n->direita->direita){
+                printf("A");
+                printTAC(n->direita->direita, 0, reg, label, param);
+                printf("B");
+                printf("t%d = t%d * 4", *reg, *reg-1);
+                *reg = *reg + 1;
+            }
+        }
         if(n->direita->tipo == CHAMADA){
-            printf("d");
             printf("\n");
             reg_ant2 = *reg - 1;
             *param = - 2;
             printTAC(n->direita, 0, reg, label, param);
-            printf("dasd ");
             lado = -1;
         }
 
@@ -74,13 +92,17 @@ void printOP(AST *n, int *reg, int first, int *label, int *param){
                     *reg = *reg + 1;
                 }
                 else{
-                    printf("t%d = pt%d", *reg, *reg-1);
+                    printf("t%d = t%d", *reg, *reg-1);
                     *reg = *reg + 1;
                 }
             }
-            //printf("e");
             printIDVALOP(n);
             
+        }
+    }
+     if(n->tipo == ID){
+        if(n->direita){
+            printf("[t%d]\n", *reg-2);
         }
     }
     if(!n->direita && !n->esquerda){
@@ -88,16 +110,15 @@ void printOP(AST *n, int *reg, int first, int *label, int *param){
         if(first){
             printf("t%d = ", *reg);
             *reg = *reg + 1;
-            //printf("r%d", reg);
         }
         printIDVALOP(n);
+        
         if(first)
             printf("\n");
     }
     
   
     printOP(n->direita, reg, lado, label, param);
-    //reg--;
     if(n->direita && n->esquerda && n->direita->tipo == OP && n->esquerda->tipo == OP){
         printf("t%d = t%d %s t%d", *reg, *reg-1, operadorToString(n->dado.operador), *reg-2);
         *reg = *reg + 1;
@@ -112,23 +133,21 @@ void printTAC(AST *n, int nivel, int *reg, int *label, int *param) {
     int skip = 0, reg1 = 0, reg2 = 0, temp = 0, temp2 = 0;
 
 
-    for (int i = 0; i < nivel; i++) printf("   ");
-
     switch (n->tipo) {
-        case VAL:     printf("(VAL %d)\n", n->dado.valor);
+        case VAL:
             if(*param > -1){
                 printf("param %d\n", n->dado.valor);
-                //*param = *param + 1; 
             }
+            
             break;
-        case ID:      printf("(ID \"%s\")\n",n->dado.id);
+        case ID:
             printf("%d", *param);
             if(*param > -1){
                 printf("param %s\n", n->dado.id);
-                //*param = *param + 1; 
             }
+           
             break;
-        case OP:      printf("(OP %d)\n", n->dado.operador);
+        case OP:
             switch(n->dado.operador){
                 case 4: 
                     printf("\n#========= Atribuicao de %s =========\n", n->esquerda->dado.id);    
@@ -138,11 +157,27 @@ void printTAC(AST *n, int nivel, int *reg, int *label, int *param) {
                         printf("%s = t%d\n", n->esquerda->dado.id, *reg-1);
                     }
                     else{
-
                         printOP(n->direita, reg, 1, label, param);
-                        //reg = printTAC(n->direita, nivel + 1, reg, 1);
                         
-                        printf("%s = t%d\n", n->esquerda->dado.id, *reg-1);
+                        if(n->esquerda->direita){
+                            temp = *reg - 1;
+                            if(n->esquerda->direita->tipo == ID){
+                                printf("t%d = %s", *reg, n->esquerda->direita->dado.id);
+                                *reg = *reg + 1;
+                            }
+                            if(n->esquerda->direita->tipo == VAL){
+                                printf("t%d = %d", *reg, n->esquerda->direita->dado.valor);
+                                *reg = *reg + 1;
+                            }
+                            printTAC(n->esquerda->direita, nivel, reg, label, param);
+                            printf("t%d = t%d * 4\n", *reg, *reg-1);
+                            *reg = *reg + 1;
+                            printf("%s[t%d] = t%d\n", n->esquerda->dado.id, *reg-1, temp);
+                        }
+                        else{
+
+                            printf("%s = t%d\n", n->esquerda->dado.id, *reg-1);
+                        }
                         //skip = 0;
                     }        
                     skip = 1;
@@ -154,9 +189,8 @@ void printTAC(AST *n, int nivel, int *reg, int *label, int *param) {
                     
                     printOP(n, reg, 1, label, param);
                     if(*param > -1){
-                        printf("%d", *param);
+                        //printf("%d", *param);
                         printf("param t%d\n", *reg-1);
-                        //*param = *param + 1; 
                     }
                     skip = 1;
                     break;
@@ -164,7 +198,7 @@ void printTAC(AST *n, int nivel, int *reg, int *label, int *param) {
                     break;
             }
             break;
-        case IF:      printf("(IF)\n");
+        case IF:
             printf("#========= IF =========\n");
             switch(n->esquerda->tipo){
                 case VAL:
@@ -190,7 +224,7 @@ void printTAC(AST *n, int nivel, int *reg, int *label, int *param) {
             skip = 1;
             
             break;
-        case IF_ELSE: printf("(IF_ELSE)\n");
+        case IF_ELSE:
             printf("#========= IF_ELSE =========\n");
             switch(n->esquerda->tipo){
                 case VAL:
@@ -219,7 +253,7 @@ void printTAC(AST *n, int nivel, int *reg, int *label, int *param) {
             printf("L_end%d:\n", temp);
             skip = 1;
             break;
-        case WHILE:   printf("(WHILE)\n");
+        case WHILE:
             printf("#========= WHILE =========\n");
             printf("L_while%d:\n", *label);
             temp = *label;
@@ -248,7 +282,7 @@ void printTAC(AST *n, int nivel, int *reg, int *label, int *param) {
             printf("L_end_while%d:\n", temp);
             skip = 1;
             break;
-        case SEQ:     printf("(SEQ)\n");
+        case SEQ:
             if(*param > -1){
                 *param = *param + 1;
                 printTAC(n->esquerda, nivel + 1, reg, label, param);
@@ -258,23 +292,18 @@ void printTAC(AST *n, int nivel, int *reg, int *label, int *param) {
 
             }
             break;
-        case FUNCAO:  printf("(FUNCAO %s)\n", n->dado.id );
+        case FUNCAO:
             printf("#========= FUNCAO =========\n");
             printf("L_%s:\n", n->dado.id);
             printTAC(n->direita, nivel + 1, reg, label, param);
             skip = 1;
             break;
-        case DECLARA: printf("(DECLARA %s)\n", n->dado.id ); break;
-        case INT: printf("(INT)\n"); break;
-        case VOID: printf("(VOID)\n"); break;
+        case DECLARA: break;
+        case INT: break;
+        case VOID: break;
         case CHAMADA: printf("(CHAMADA %s)\n", n->dado.id );
-            // if(*param > 0){
-            //     printf("pa%d", *param);
-            //     //temp = *param;
-            //     temp2 = *param;
-            //     *param = 0;
-
-            // }
+            printf("\n#========= CHAMADA =========\n");
+            
             if(*param == -2){
                 temp = 1;
             }
@@ -290,21 +319,15 @@ void printTAC(AST *n, int nivel, int *reg, int *label, int *param) {
             }
 
             printTAC(n->direita, nivel + 1, reg, label, param);
-            //printOP(n->direita, reg, 1);
             
             if(reg1 == 1 && temp != 1){
-                // if(reg1 == 0){
-                //     printf("t%d = ", *reg);
-                //     *reg = *reg + 1;
-                // }
-                printf("%d %d %d %d", reg1, temp, reg2, *param);
+                
                 printf("call %s, %d\n", n->dado.id, *param+1);
             }
             else{
                 if(n->direita->tipo != ID && n->direita->tipo != OP && n->direita->tipo != VAL && n->direita->tipo != SEQ)
-                    printf("eparam t%d %d %d %d %d \n", *reg-1, *param, temp, reg1, reg2);
-                //*param += *reg - reg2 - 1;
-                //printf("%d, %d", *reg, reg2);
+                    printf("param t%d\n", *reg-1);
+                
                 printf("t%d = call %s, %d\n", *reg, n->dado.id, *param+1);
                 *reg = *reg + 1;
             }
@@ -313,36 +336,33 @@ void printTAC(AST *n, int nivel, int *reg, int *label, int *param) {
             }
             if(reg1 == 1)
                 *param = -1;
-            // else
-            //     *param = 0;
-            // if(temp2 > 0 && reg1 != 1){
-            //     printf("temp2%d", temp2);
-            //     *param = temp2;
-            // }
             
             skip = 1;
             break;
-        case PARAM:   printf("(PARAM %s)\n", n->dado.id ); break;
-        case RETURN:  printf("(RETURN)\n");
-            printf("#========= RETURN =========\n");
-            switch(n->esquerda->tipo){
-                case VAL:
+        case PARAM: break;
+        case RETURN:
+            if(n->esquerda){
+                switch(n->esquerda->tipo){
+                    case VAL:
                     printf("Return %d\n", n->esquerda->dado.valor);
                     break;
-                case ID:
+                    case ID:
                     printf("Return %s\n", n->esquerda->dado.id);
                     break;
-                case OP:
+                    case OP:
                     printTAC(n->esquerda, nivel + 1, reg, label, param);
                     printf("Return t%d\n", *reg-1);
                     break;
+                }
+            }
+            else{
+                printf("Return\n");
             }
             skip = 1;
             break;
         default:      printf("(NODE %d)\n", n->tipo); break;
     }
 
-    //if(n->tipo != OP){
    if(skip == 0){
 
         printTAC(n->esquerda, nivel + 1, reg, label, param);
@@ -350,9 +370,6 @@ void printTAC(AST *n, int nivel, int *reg, int *label, int *param) {
         printTAC(n->direita, nivel + 1, reg, label, param);
    }
    
-    //printf("ret%d", reg);
-    
-
-    //}
+  
 }
 
--- src/teste.c
+++ src/teste.c
@@ -1,37 +1,12 @@
-/*
 int gcd(int u, int v)
 {
-    int x;
-    if (v == 0) return u;
-    else return gcd(v, u - (u/v) * v);
+if (v == 0) return u;
+else return gcd(v, u - (u/v) * v);
 }
-*/
-int funcao(int x, int y){
-    x = x + 2;
-    return x + 1 + x / 3;
-}
-
-int teste2(int x, int y, int z){
-    return x;
-}
-
-/*Lex comentário :D*/
-
 void main(void)
 {
-int x;
-int y;
-/*y = 2 * 1 + 3 / 5 -2;*/
+int x; int y;
+x = 2;
+y = 2;
 
-
-/*
-output(gcd(x[0], y + 2 + x));
-*/
-/*
-output(teste(x, y));
-*/
-x = funcao(10 + funcao(4+x)) +funcao(1,4,0);
-x = funcao(10 - x, y-3+5, 10) + 3 +funcao(1,4,0);
-y = teste(x+2, 5 + teste(x+teste(y)) + 4);
-return 1 + 2;
 }
