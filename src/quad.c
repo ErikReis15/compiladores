84e482640a14750f66ebf241adbb0667f17957f9
felipe.alves16 <felipe.alves16@unifesp.br>
2025-12-14 23:53:57 -0300

    e

--- src/quad.c
+++ src/quad.c
@@ -48,11 +48,7 @@ void printOP(AST *n, int *reg, int first, int *label, int *param){
     int reg_right = *reg - 1;
 
     if(n->tipo == OP){
-        printf("t%d = t%d %s t%d\n",
-               *reg,
-               reg_left,
-               operadorToString(n->dado.operador),
-               reg_right);
+        printf("t%d = t%d %s t%d\n", *reg, reg_left, operadorToString(n->dado.operador), reg_right);
         (*reg)++;
     }
 
@@ -91,10 +87,7 @@ void printTAC(AST *n, int nivel, int *reg, int *label, int *param){
                 printOP(n->esquerda->direita, reg, 1, label, param);
                 printf("t%d = t%d * 4\n", *reg, *reg - 1);
                 (*reg)++;
-                printf("%s[t%d] = t%d\n",
-                       n->esquerda->dado.id,
-                       *reg - 1,
-                       temp);
+                printf("%s[t%d] = t%d\n", n->esquerda->dado.id, *reg - 1, temp);
             }
             else{
                 printf("%s = t%d\n", n->esquerda->dado.id, temp);
